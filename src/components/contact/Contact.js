52435f24b3d9947dacdcde6afb853f13dbc8e3ec
“marcia.dasilva” <marcia.dasilva@rewardops.com>
2020-10-03 23:34:20 -0400

    adding contact form to boilerplate

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Layout from '../../layouts';
 import './Contact.css';
 
-const Contact = () => (
-  <Layout>
-    <section className='contact' />
-  </Layout>
-);
+const ContactContent = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(email) {
+      alert(`Sending email to ${email}!`);
+    }
+  }
+  return (
+    <section className="contact">
+      <form onSubmit={handleSubmit}>
+        <div className="contact__input-group">
+          <label>Name</label>
+          <input name="Name" value={name} onChange={e => setName(e.target.value)} />
+        </div>
+        <div className="contact__input-group">
+          <label>Email</label>
+          <input type="email" name="Email" value={email} onChange={e => setEmail(e.target.value)} />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </section>
+  );
+};
+
+const Contact = () => <Layout><ContactContent /></Layout>;
 
 export default Contact;
